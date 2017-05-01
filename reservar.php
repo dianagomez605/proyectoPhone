928a79f4a743050ff3bae248c2706d5c6404f7ff
dianagomez605 <diana.gomez605@gmail.com>
2017-05-01 16:38:31 -0500

    Version 1

---
+++ reservar.php
@@ -0,0 +1,25 @@
+<?php
+ 
+ include("conexion.php");
+
+$cedula = $_POST["cedula"];
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellido"];
+$placa = $_POST["placa"];
+
+$sentencia = "insert into reserva(nombre, apellidos, cedula, placa) 
+values('".$nombre."', '".$apellidos."','".$cedula."','".$placa."')";
+
+$query = mysqli_query($conexion,$sentencia);
+
+if($query){
+	echo "Se creó la reserva del carro ".$placa;
+}else{
+	echo "No fue posible crear la reserva";
+}
+
+mysqli_close($conexion);
+
+
+
+?>
